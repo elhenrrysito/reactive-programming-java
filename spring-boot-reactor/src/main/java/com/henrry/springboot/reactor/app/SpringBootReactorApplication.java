5032f5517f5d4af291bd553b9b0671cfc613c3ad
Henrry <84257069+elhenrrysito@users.noreply.github.com>
2021-10-18 17:39:28 -0300

    Add files via upload

---
+++ spring-boot-reactor/src/main/java/com/henrry/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,19 @@
+package com.henrry.springboot.reactor.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Henrry", "Sonia", "Sareth", "Leonel");
+	}
+}
